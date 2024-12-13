4333ef72cc41f79e3f916e6fab8537775a2fc825
DMC <surendra.singh20.in@gmail.com>
2024-12-13 12:46:37 +0530

    Add files via upload

---
+++ src/main/java/com/gl/eirs/hlrfulldump/configuration/AppConfig.java
@@ -0,0 +1,51 @@
+package com.gl.eirs.hlrfulldump.configuration;
+
+
+import com.gl.eirs.hlrfulldump.HlrDumpProcessorMain;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+@Configuration
+@Data
+@Service
+public class AppConfig {
+//    @Bean
+//    public HlrDumpProcessorMain hlrDumpProcessorMain() {
+//        return new HlrDumpProcessorMain();
+//    }
+
+    @Value("${batch.count}")
+    int batchCount;
+
+    @Value("${file.separator.parameter}")
+    String fileSeparator;
+
+    @Value("${operator}")
+    String operator;
+
+    @Value("${input.file.path}")
+    String deltaFilePath;
+
+    @Value("${eirs.alert.url}")
+    String alertUrl;
+
+
+//    @Value("${password.decryptor}")
+//    String passwordDecryptor;
+
+    @Value("${spring.datasource.url}")
+    String dbUrl;
+
+    @Value("${spring.datasource.driver-class-name}")
+    String jdbcDriver;
+
+    @Value("${spring.datasource.password}")
+    String springDatasourcePassword;
+
+    @Value("${spring.datasource.username}")
+    String dbUsername;
+
+}
